04369189efa32d33ccc1ac1b3380579305b91dbf
Maria Treier <mariatraier@gmail.com>
2025-05-22 19:02:27 +0200

    feat: implement memberTypes schema

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import { graphql, GraphQLEnumType, GraphQLFloat, GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import { MemberTypeId } from './types/member-type.js';
+import { ResolverContext } from './types/context.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -16,9 +18,10 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req) {
       return graphql({
-        schema: schema,
+        schema,
         source: req.body.query,
-        variableValues: req.body.variables
+        variableValues: req.body.variables,
+        contextValue: { prisma }
       });
     },
   });
@@ -26,12 +29,38 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
-    name: 'TestQuery',
+    name: 'MemberTypesQuery',
     fields: {
-      hello: {
-        type: GraphQLString,
-        resolve: async () => {
-          return 'Hello world!';
+      // hello: {
+      //   type: GraphQLString,
+      //   resolve: async () => {
+      //     return 'Hello world!';
+      //   }
+      // },
+      memberTypes: {
+        type: new GraphQLList(new GraphQLObjectType({
+          name: 'MemberType',
+          fields: {
+            id: { type: new GraphQLEnumType({
+                name: 'MemberTypeId',
+                values: {
+                  [MemberTypeId.BASIC]:{ value: MemberTypeId.BASIC},
+                  [MemberTypeId.BUSINESS]:{ value: MemberTypeId.BUSINESS},
+                }
+              })
+            },
+            discount: {
+              type: new GraphQLNonNull(GraphQLFloat),
+            },
+            postsLimitPerMonth: {
+              type: new GraphQLNonNull(GraphQLInt)
+            }
+
+          }
+        })),
+        resolve: async (_parent: unknown, _args: unknown, context: ResolverContext) => {
+          const data = await context.prisma.memberType.findMany();
+          return data;
         }
       }
     }
---
+++ src/routes/graphql/types/member-type.ts
@@ -0,0 +1,13 @@
+// export type MemberType = {
+//   id: MemberTypeId!
+//   discount: Float!
+//   postsLimitPerMonth: Int!
+// }
+
+export enum MemberTypeId {
+  BASIC = 'BASIC',
+  BUSINESS = 'BUSINESS',
+}
+
+
+// export const
